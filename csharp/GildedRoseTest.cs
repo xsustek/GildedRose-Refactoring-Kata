fa60c11a80f6e9669e8293e9651dcdb7164bf3c9
Milan Šůstek <milan.sustek@gmail.com>
2018-05-28 00:51:01 +0200

    testy podle kontraktu

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -52,5 +52,116 @@ namespace csharp
             Assert.That(items.Single(i => i.Name == "Good").Quality, Is.EqualTo(11));
             Assert.That(items.Single(i => i.Name == "Good2").Quality, Is.EqualTo(7));
         }
+
+        [Test]
+        public void QualityDegradationWithSellInZero()
+        {
+            IList<Item> items = new List<Item>
+            {
+                new Item {Name = "Good", SellIn = 0, Quality = 12},
+            };
+            GildedRose app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.That(items[0].Quality, Is.EqualTo(10));
+            
+        }
+
+        [Test]
+        public void QualityDegradationNeverNegative()
+        {
+            IList<Item> items = new List<Item>
+            {
+                new Item {Name = "Good", SellIn = 7, Quality = 0},
+            };
+            GildedRose app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.That(items[0].Quality, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void AgedBrieIncreaseQuality()
+        {
+            IList<Item> items = new List<Item>
+            {
+                new Item {Name = "Aged Brie", SellIn = 10, Quality = 0},
+            };
+            GildedRose app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.That(items[0].Quality, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void AgedBrieIncreaseQualityNotMoreThan50()
+        {
+            IList<Item> items = new List<Item>
+            {
+                new Item {Name = "Aged Brie", SellIn = 10, Quality = 49},
+            };
+            GildedRose app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.That(items[0].Quality, Is.EqualTo(50));
+            app.UpdateQuality();
+            Assert.That(items[0].Quality, Is.EqualTo(50));
+        }
+
+        [Test]
+        public void BackstagePassQualityIncrease()
+        {
+            IList<Item> items = new List<Item>
+            {
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 11, Quality = 20},
+            };
+            GildedRose app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.That(items[0].Quality, Is.EqualTo(21));
+        }
+
+        [Test]
+        [TestCase(10, 22)]
+        [TestCase(9, 22)]
+        [TestCase(8, 22)]
+        [TestCase(7, 22)]
+        [TestCase(6, 22)]
+        public void BackstagePassQualityIncreaseSellInBy2(int sellIn, int expectedQuality)
+        {
+            IList<Item> items = new List<Item>
+            {
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = sellIn, Quality = 20},
+            };
+            GildedRose app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.That(items[0].Quality, Is.EqualTo(expectedQuality));
+        }
+
+        [Test]
+        [TestCase(5, 23)]
+        [TestCase(4, 23)]
+        [TestCase(3, 23)]
+        [TestCase(2, 23)]
+        [TestCase(1, 23)]
+        public void BackstagePassQualityIncreaseSellInBy3(int sellIn, int expectedQuality)
+        {
+            IList<Item> items = new List<Item>
+            {
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = sellIn, Quality = 20},
+            };
+            GildedRose app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.That(items[0].Quality, Is.EqualTo(expectedQuality));
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(-1)]
+        public void BackstagePassQualityZeroWhenZeroOrNegativeSellIn(int sellIn)
+        {
+            IList<Item> items = new List<Item>
+            {
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = sellIn, Quality = 20},
+            };
+            GildedRose app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.That(items[0].Quality, Is.EqualTo(0));
+        }
     }
 }
