3a868445608c5aebc567031f765b2517af5db057
Milan Šůstek <milan.sustek@gmail.com>
2018-05-28 02:16:10 +0200

    backstage pass impl

---
+++ csharp/GildedRoseBackstagePass.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace csharp
+{
+    public class GildedRoseBackstagePass : GildedRoseBase
+    {
+        protected override void UpdateQuality(Item item)
+        {
+            if (item.SellIn <= 0)
+            {
+                item.Quality = 0;
+            }
+            else if (item.Quality == 50)
+            {
+            }
+            else if (item.SellIn <= 5)
+            {
+                item.Quality += 3;
+            }
+            else if (item.SellIn <= 10)
+            {
+                item.Quality += 2;
+            }
+            else
+            {
+                item.Quality++;
+            }
+        }
+    }
+}
--- csharp/GildedRoseFactory.cs
+++ csharp/GildedRoseFactory.cs
@@ -3,12 +3,15 @@
     public class GildedRoseFactory
     {
         private const string AgedBrie = "Aged Brie";
+        private const string BackstagePass = "Backstage passes to a TAFKAL80ETC concert";
         public static IGildedRose GetGildedRose(Item item)
         {
             switch (item.Name)
             {
                 case AgedBrie:
                     return new GildedRoseAgedBrie();
+                case BackstagePass:
+                    return new GildedRoseBackstagePass();
                 default:
                     return new GildedRoseBase();
             }
