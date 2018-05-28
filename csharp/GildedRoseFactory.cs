81308fc26c36a3ee041e2a1efa4dd92e4f399e4b
Milan Šůstek <milan.sustek@gmail.com>
2018-05-28 02:16:54 +0200

    Sulfuras impl

--- csharp/GildedRoseFactory.cs
+++ csharp/GildedRoseFactory.cs
@@ -4,6 +4,7 @@
     {
         private const string AgedBrie = "Aged Brie";
         private const string BackstagePass = "Backstage passes to a TAFKAL80ETC concert";
+        private const string Sulfuras = "Sulfuras, Hand of Ragnaros";
         public static IGildedRose GetGildedRose(Item item)
         {
             switch (item.Name)
@@ -12,6 +13,8 @@
                     return new GildedRoseAgedBrie();
                 case BackstagePass:
                     return new GildedRoseBackstagePass();
+                case Sulfuras:
+                    return new GildedRoseSulfuras();
                 default:
                     return new GildedRoseBase();
             }
---
+++ csharp/GildedRoseSulfuras.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace csharp
+{
+    public class GildedRoseSulfuras : GildedRoseBase
+    {
+        protected override void UpdateQuality(Item item)
+        {
+        }
+
+        protected override void UpdateSellIn(Item item)
+        {
+        }
+    }
+}
