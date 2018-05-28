75eeb10154d948a8acd467865942511584a4a0a0
Milan Šůstek <milan.sustek@gmail.com>
2018-05-28 02:15:01 +0200

    aged brie impl

---
+++ csharp/GildedRoseAgedBrie.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace csharp
+{
+    public class GildedRoseAgedBrie : GildedRoseBase
+    {
+        protected override void UpdateQuality(Item item)
+        {
+            if(item.Quality == 50) return;
+            item.Quality++;
+        }
+    }
+}
--- csharp/GildedRoseFactory.cs
+++ csharp/GildedRoseFactory.cs
@@ -2,10 +2,13 @@
 {
     public class GildedRoseFactory
     {
+        private const string AgedBrie = "Aged Brie";
         public static IGildedRose GetGildedRose(Item item)
         {
             switch (item.Name)
             {
+                case AgedBrie:
+                    return new GildedRoseAgedBrie();
                 default:
                     return new GildedRoseBase();
             }
